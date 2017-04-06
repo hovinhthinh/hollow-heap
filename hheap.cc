f88f31a7bb80c69c205a55d5bf7bb11f4bc0d976
Ho Vinh Thinh <hovinhthinh@lap-05-65>
2017-04-06 02:09:18 +0200

    improve performance by not using smart pointers and using vector instead of linked list

--- hheap.cc
+++ hheap.cc
@@ -3,16 +3,16 @@
 
 template<typename I, typename V>
 Node<I, V>::Node(I _id, V _value) :
-		id(_id), pvalue(new V(_value)), rank(0), next_sibling(nullptr), head_childs(nullptr), is_root(true) {
+		id(_id), value(_value), rank(0), is_hollow(false), is_root(true), next_sibling(nullptr), head_childs(nullptr) {
 }
 
 template<typename I, typename V, typename C>
-std::list<Node<I, V>*>& HollowHeap<I, V, C>::RootsAtRank(uint32_t rank) {
+inline std::vector<Node<I, V>*>& HollowHeap<I, V, C>::RootsAtRank(uint32_t rank) {
 	if (roots_of_rank_.size() <= rank) {
 		uint32_t old_size = roots_of_rank_.size();
 		roots_of_rank_.resize(rank + 1, nullptr);
 		for (uint32_t i = old_size; i <= rank; ++i) {
-			roots_of_rank_[i] = new std::list<Node<I, V>*>();
+			roots_of_rank_[i] = new std::vector<Node<I, V>*>();
 		}
 	}
 
@@ -36,7 +36,7 @@ bool HollowHeap<I, V, C>::Push(I id, V value) {
 	RootsAtRank(0).emplace_back(new_node);
 
 	++size_;
-	if (min_id_ == nullptr || comparator_(value, *(min_id_->pvalue))) {
+	if (min_id_ == nullptr || comparator_(value, min_id_->value)) {
 		min_id_ = new_node;
 	}
 	return true;
@@ -44,13 +44,13 @@ bool HollowHeap<I, V, C>::Push(I id, V value) {
 
 template<typename I, typename V, typename C>
 std::pair<I, V> HollowHeap<I, V, C>::Top() const {
-	return std::make_pair(min_id_->id, *(min_id_->pvalue));
+	return std::make_pair(min_id_->id, min_id_->value);
 }
 
 template<typename I, typename V, typename C>
-Node<I, V>* HollowHeap<I, V, C>::link(Node<I, V> *r1, Node<I, V> *r2) {
+inline Node<I, V>* HollowHeap<I, V, C>::link(Node<I, V> *r1, Node<I, V> *r2) {
 	Node<I, V>* temp;
-	if (comparator_(*(r1->pvalue), *(r2->pvalue))) {
+	if (comparator_(r1->value, r2->value)) {
 		temp = r1->head_childs;
 		r1->head_childs = r2;
 		r2->next_sibling = temp;
@@ -72,14 +72,15 @@ void HollowHeap<I, V, C>::Pop() {
 	Node<I, V> *temp, *temp_2;
 	// remove id & make top hollow
 	node_map_.erase(min_id_->id);
-	min_id_->pvalue.reset();
+	min_id_->is_hollow = true;
 
 	// recursively remove hollow roots
-	for (auto it1 = roots_of_rank_.rbegin(); it1 != roots_of_rank_.rend(); ++it1) {
-		std::list<Node<I, V>*> &l = **it1;
-		for (auto it2 = l.begin(); it2 != l.end();) {
-			if (!((*it2)->pvalue)) { // hollow node
-				Node<I, V> *current = (*it2)->head_childs;
+	for (auto it = roots_of_rank_.rbegin(); it != roots_of_rank_.rend(); ++it) {
+		std::vector<Node<I, V>*> &l = **it;
+		uint32_t non_hollow = 0;
+		for (uint32_t i = 0; i < l.size(); ++i) {
+			if (l[i]->is_hollow) { // hollow node
+				Node<I, V> *current = l[i]->head_childs;
 				while (current) {
 					roots_of_rank_[current->rank]->emplace_back(current);
 					current->is_root = true;
@@ -87,27 +88,27 @@ void HollowHeap<I, V, C>::Pop() {
 					current = current->next_sibling;
 					temp->next_sibling = nullptr;
 				}
-				delete (*it2);
-				it2 = l.erase(it2);
+				delete l[i];
 			} else {
-				++it2;
+				l[non_hollow++] = l[i];
 			}
 		}
+		l.resize(non_hollow);
 	}
 
 	// re-link roots and shrink roots vector
 	int32_t max_rank = -1;
 	for (uint32_t i = 0; i < roots_of_rank_.size(); ++i) {
-		std::list<Node<I, V>*> &l = *roots_of_rank_[i];
+		std::vector<Node<I, V>*> &l = *roots_of_rank_[i];
 		if (l.size()) {
 			max_rank = l.front()->rank;
 		}
 		uint32_t num_links = l.size() >> 1;
 		while (num_links--) {
-			temp = l.front();
-			l.pop_front();
-			temp_2 = l.front();
-			l.pop_front();
+			temp = l.back();
+			l.pop_back();
+			temp_2 = l.back();
+			l.pop_back();
 			temp = link(temp, temp_2);
 			RootsAtRank(temp->rank).emplace_back(temp);
 		}
@@ -121,10 +122,10 @@ void HollowHeap<I, V, C>::Pop() {
 	// update min-id
 	min_id_ = nullptr;
 	for (auto it = roots_of_rank_.begin(); it != roots_of_rank_.end(); ++it) {
-		std::list<Node<I, V>*> &l = **it;
+		std::vector<Node<I, V>*> &l = **it;
 		if (l.size()) {
-			if (min_id_ == nullptr || comparator_(*(l.front()->pvalue), *(min_id_->pvalue))) {
-				min_id_ = l.front();
+			if (min_id_ == nullptr || comparator_(l[0]->value, min_id_->value)) {
+				min_id_ = l[0];
 			}
 		}
 	}
@@ -137,22 +138,22 @@ void HollowHeap<I, V, C>::Pop() {
 template<typename I, typename V, typename C>
 bool HollowHeap<I, V, C>::Update(I id, V value) {
 	auto i = node_map_.find(id);
-	if (i == node_map_.end() || !comparator_(value, *(i->second->pvalue))) {
+	if (i == node_map_.end() || !comparator_(value, i->second->value)) {
 		return false;
 	}
 
 	Node<I, V> *current_node = i->second;
 	// if id is root
 	if (current_node->is_root) {
-		current_node->pvalue.reset(new V(value));
-		if (comparator_(*(current_node->pvalue), *(min_id_->pvalue))) {
+		current_node->value = value;
+		if (comparator_(current_node->value, min_id_->value)) {
 			min_id_ = current_node;
 		}
 		return true;
 	}
 
 	// make current node hollow
-	current_node->pvalue.reset();
+	current_node->is_hollow = true;
 
 	// build new node
 	Node<I, V> *new_node = new Node<I, V>(id, value);
@@ -168,7 +169,7 @@ bool HollowHeap<I, V, C>::Update(I id, V value) {
 	RootsAtRank(new_node->rank).emplace_back(new_node);
 
 	// update min-id
-	if (comparator_(*(new_node->pvalue), *(min_id_->pvalue))) {
+	if (comparator_(new_node->value, min_id_->value)) {
 		min_id_ = new_node;
 	}
 
@@ -199,7 +200,7 @@ void HollowHeap<I, V, C>::DeleteNodeRecursively(Node<I, V> *node) {
 template<typename I, typename V, typename C>
 HollowHeap<I, V, C>::~HollowHeap() {
 	for (auto it = roots_of_rank_.begin(); it != roots_of_rank_.end(); ++it) {
-		std::list<Node<I, V>*> &l = **it;
+		std::vector<Node<I, V>*> &l = **it;
 		for (const auto &n : l) {
 			DeleteNodeRecursively(n);
 		}
--- hheap.h
+++ hheap.h
@@ -5,7 +5,7 @@
 // - Push(): O(1)
 // - Top(): O(1)
 // - Pop(): O(log N)
-// - UpdateIey(): O(1)
+// - Update(): O(1)
 
 // using hollow heap could reduce the complexity of dijktra to O(M + N log N)
 // very efficient for dense graph
@@ -14,8 +14,6 @@
 #define _HOLLOW_HEAP
 
 #include <functional>
-#include <list>
-#include <memory>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -28,14 +26,14 @@ namespace {
 template<typename I, typename V>
 struct Node {
 	I id;
-	std::unique_ptr<V> pvalue;
+	V value;
 
 	uint32_t rank;
+	bool is_hollow;
+	bool is_root;
 
 	Node<I, V> *next_sibling; // siblings are stored in decreasing order of rank
-	Node<I, V> *head_childs; // point to the first child of childs list
-
-	bool is_root;
+	Node<I, V> *head_childs; // point to the first child of childs vector
 
 	Node(I _id, V _value);
 };
@@ -48,10 +46,10 @@ private:
 	C comparator_;
 	Node<I, V> *min_id_;
 
-	std::vector<std::list<Node<I, V>*>*> roots_of_rank_;
+	std::vector<std::vector<Node<I, V>*>*> roots_of_rank_;
 	std::unordered_map<I, Node<I, V>*> node_map_;
 
-	std::list<Node<I, V>*>& RootsAtRank(uint32_t rank);
+	std::vector<Node<I, V>*>& RootsAtRank(uint32_t rank);
 
 	Node<I, V>* link(Node<I, V> *r1, Node<I, V> *r2);
 
